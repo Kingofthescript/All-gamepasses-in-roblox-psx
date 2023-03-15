8ab80a077104ddb7e4c142df774942d7e04c2c5d
Kingofthescript <121552724+Kingofthescript@users.noreply.github.com>
2023-03-15 19:36:52 +0000

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Kingofthescript/All-gamepasses-config/main/main.lua?token=GHSAT0AAAAAAB7HQVPARHQXTMKHHEG3OWOMZASCNCQ", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kingofthescript/All-gamepasses-config/main/main.lua", true))()
